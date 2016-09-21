1c5a280fe04db9e15d81f20b627badf0ad704b88
Ryan Nevius <ryanevius@gmail.com>
2016-09-21 15:38:39 -0600

    Add fixtures. Update models, views, and templates.

--- eventio/urls.py
+++ eventio/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from events.views import index
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='home'),
 ]
--- events/models.py
+++ events/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    capacity = models.PositiveSmallIntegerField(blank=True, null=True)
+    name = models.CharField(max_length=255)
     description = models.TextField()
+    start_time = models.DateTimeField()
     # image = models.ImageField()
     location = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2, max_digits=6)
-    start_time = models.DateTimeField()
+    price = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=6)
+    capacity = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
