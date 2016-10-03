a25a0be607df97568de8b73040266513c1bcc8f6
Ryan Nevius <ryanevius@gmail.com>
2016-10-03 16:00:58 -0600

    Refactor templates. Add base styles.

--- eventio/urls.py
+++ eventio/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from events.views import index
+from events.views import index, event_detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='home'),
+    url(r'^event/(?P<pk>\d+)/$', event_detail, name='event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Event
 
-# Create your views here.
 def index(request):
     events = Event.objects.all()
     return render(request, 'index.html', {'events': events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'event_detail.html', {'event': event})
