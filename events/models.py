01f7f65402b11dffb1ae7835e05ad9fa34b3ebc3
Ryan Nevius <ryanevius@gmail.com>
2016-10-12 21:28:11 -0600

    Remove unused template tags and commented model fields.

--- events/models.py
+++ events/models.py
@@ -5,7 +5,6 @@ class Event(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     start_time = models.DateTimeField()
-    # image = models.ImageField()
     location = models.CharField(max_length=255)
     price = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=6)
     capacity = models.PositiveSmallIntegerField(blank=True, null=True)
