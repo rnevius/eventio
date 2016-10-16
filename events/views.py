976e5e7dfcb9b0abbf70d8161ae4485959f14da4
Ryan Nevius <ryanevius@gmail.com>
2016-10-16 17:51:59 -0600

    Add datetime picker to frontent Add Event form. Fix redirect bug in event_add view.

--- events/views.py
+++ events/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import AddEventForm
 from .models import Event
+
 
 def index(request):
     events = Event.objects.all()
     return render(request, 'index.html', {'events': events})
 
+
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     return render(request, 'event_detail.html', {'event': event})
+
 
 def event_add(request):
     if request.method == 'POST':
         form = AddEventForm(request.POST)
         if form.is_valid():
             event = form.save()
-            return redirect('event_detail', pk=event.pk)
+            return redirect('event', pk=event.pk)
     else:
         form = AddEventForm()
     return render(request, 'event_add.html', {'form': form})
