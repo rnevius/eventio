fa2cc02ea1dd2b6a1bb87973e9e30194c6ad6f9b
Ryan Nevius <ryanevius@gmail.com>
2016-10-19 22:06:08 -0600

    Add frontend form and form logic for adding events.

---
+++ events/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Event
+
+
+class AddEventForm(forms.ModelForm):
+
+    class Meta:
+        model = Event
+        fields = ('name', 'description',
+                  'start_time', 'location',
+                  'price', 'capacity')
--- events/views.py
+++ events/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 
+from .forms import AddEventForm
 from .models import Event
 
 def index(request):
     events = Event.objects.all()
     return render(request, 'index.html', {'events': events})
 
+
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     return render(request, 'event_detail.html', {'event': event})
+
+
+def event_add(request):
+    if request.method == 'POST':
+        form = AddEventForm(request.POST)
+    else:
+        form = AddEventForm()
+    return render(request, 'event_add.html', {'form': form})
