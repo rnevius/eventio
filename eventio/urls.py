17f556ffcae9bbd1ef8cff16e4140113d9848d3d
Ryan Nevius <ryanevius@gmail.com>
2016-10-24 21:31:26 -0600

    Simplify views and URLs from last commit.

--- eventio/urls.py
+++ eventio/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import login, logout
+
 from accounts.views import register_user
 from events.views import index, event_detail, event_add
 
@@ -10,11 +11,9 @@ urlpatterns = [
     url(r'^event/(?P<pk>\d+)/$', event_detail, name='event'),
     url(r'^event/add/$', event_add, name='event_add'),
     url(
-        r'^login/$',
-        auth_views.login,
-        {'template_name': 'accounts/login.html'},
-        name='login'
+        r'^login/$', login,
+        {'template_name': 'accounts/login.html'}, name='login'
     ),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^register/$', register_user, name='register'),
-    url('^', include('django.contrib.auth.urls')),
 ]
