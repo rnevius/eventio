387b545ca2cd9754fd277c36b83fa1886a02026c
Ryan Nevius <ryanevius@gmail.com>
2016-11-02 15:55:31 -0600

    Add event delete view. Implement messages framework.

--- eventio/urls.py
+++ eventio/urls.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
 from accounts.views import register_user
-from events.views import index, event_detail, event_add, event_edit, event_register
+from events.views import (index, event_delete, event_detail,
+    event_add, event_edit, event_register)
 
 urlpatterns = [
     url(r'^$', index, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^event/add/$', event_add, name='event_add'),
     url(r'^event/(?P<pk>\d+)/$', event_detail, name='event'),
+     url(r'^event/(?P<pk>\d+)/delete/$', event_delete, name='event_delete'),
     url(r'^event/(?P<pk>\d+)/register/$', event_register, name='event_register'),
     url(r'^event/(?P<pk>\d+)/edit/$', event_edit, name='event_edit'),
     url(
--- events/views.py
+++ events/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
@@ -16,15 +17,30 @@ def event_detail(request, pk):
     return render(request, 'event_detail.html', {'event': event})
 
 
+@login_required
 def event_add(request):
     if request.method == 'POST':
         form = AddEventForm(request.POST)
         if form.is_valid():
-            event = form.save()
+            event = form.save(commit=False)
+            event.owner = request.user
+            event.save()
             return redirect('event', pk=event.pk)
     else:
         form = AddEventForm()
     return render(request, 'event_add.html', {'form': form})
+
+
+@login_required
+def event_delete(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    if request.user != event.owner:
+        raise PermissionDenied
+
+    event.delete()
+    messages.success(request, 'Event Deleted!')
+    return redirect('/')
 
 
 @login_required
