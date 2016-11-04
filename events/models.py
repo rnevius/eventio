0b4fa5ad597ee7cea22581a43f046701beaf60a1
Ryan Nevius <ryanevius@gmail.com>
2016-11-04 11:38:29 -0600

    Add event update and delete views, templates, and URLs. Add logic to enforce logged-in users for certain views.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Event(models.Model):
     description = models.TextField()
     start_time = models.DateTimeField()
     location = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, related_name='authored_events')
     price = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=6)
     capacity = models.PositiveSmallIntegerField(blank=True, null=True)
 
--- events/views.py
+++ events/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 from .forms import AddEventForm
 from .models import Event
@@ -13,12 +16,44 @@ def event_detail(request, pk):
     return render(request, 'event_detail.html', {'event': event})
 
 
+@login_required
 def event_add(request):
     if request.method == 'POST':
         form = AddEventForm(request.POST)
         if form.is_valid():
-            event = form.save()
+            event = form.save(commit=False)
+            event.owner = request.user
+            event.save()
             return redirect('event', pk=event.pk)
     else:
         form = AddEventForm()
     return render(request, 'event_add.html', {'form': form})
+
+
+@login_required
+def event_delete(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    if request.user != event.owner:
+        raise PermissionDenied
+
+    event.delete()
+    messages.success(request, 'Event Deleted')
+    return redirect('/')
+
+
+@login_required
+def event_edit(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    if request.user != event.owner:
+        raise PermissionDenied
+
+    if request.method == 'POST':
+        form = AddEventForm(request.POST, instance=event)
+        if form.is_valid():
+            event = form.save()
+            return redirect('event', pk=event.pk)
+    else:
+        form = AddEventForm(instance=event)
+    return render(request, 'event_edit.html', {'form': form})
